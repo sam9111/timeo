d2c99de653ab09a3cb9e2fc9d8d6500994147c5b
Samyuktha Ganeshkumar <samyuktha19095@cse.ssn.edu.in>
2024-01-05 21:57:37 +0530

    working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,53 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
-use tauri_plugin_positioner::{Position, WindowExt};
-
 #[tauri::command]
 async fn focus_window(window: tauri::Window) {
     window.show().unwrap();
     window.set_focus().unwrap();
 }
-
 fn main() {
-    let system_tray_menu = SystemTrayMenu::new();
     tauri::Builder::default()
-        .plugin(tauri_plugin_positioner::init())
-        .system_tray(SystemTray::new().with_menu(system_tray_menu))
-        .on_system_tray_event(|app, event| {
-            tauri_plugin_positioner::on_tray_event(app, &event);
-            match event {
-                SystemTrayEvent::LeftClick {
-                    position: _,
-                    size: _,
-                    ..
-                } => {
-                    let window = app.get_window("main").unwrap();
-                    // use TrayCenter as initial window position
-                    let _ = window.move_window(Position::TrayLeft);
-                    if window.is_visible().unwrap() {
-                        window.hide().unwrap();
-                    } else {
-                        window.show().unwrap();
-                        window.set_focus().unwrap();
-                    }
-                }
-
-                _ => {}
-            }
-        })
         .invoke_handler(tauri::generate_handler![focus_window])
-        .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::Focused(is_focused) => {
-                // detect click outside of the focused window and hide the app
-                if !is_focused {
-                    event.window().hide().unwrap();
-                }
-            }
-            _ => {}
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
